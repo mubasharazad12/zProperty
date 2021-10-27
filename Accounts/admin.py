57f305478fdb5c5c1f4a3b960114fd0451842970
Usman <uuusman872@gmail.com>
2021-10-27 16:17:44 +0500

    Added Question Answer Feature

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Question, Answers
+
 
 # Register your models here.
+
+class AnswersAdmin(admin.TabularInline):
+    model = Answers
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswersAdmin]
+
+
+admin.site.register(Question, QuestionAdmin)
--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Question = models.TextField(null=True)
+
+
+class Answers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField(null=True)
