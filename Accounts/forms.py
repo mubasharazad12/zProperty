6307a7d89ece5436aab611b45ffaa9689adf904a
mubasharazad12 <mubasharazad3@gmail.com>
2021-11-12 16:56:22 +0500

    contact form update + minor desgin changes

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -11,12 +11,65 @@ class CreateUserForm(UserCreationForm):
 
 
 class QuestionForm(forms.ModelForm):
+    
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "First Name"
+    }))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Last Name"
+    }))
+    email = forms.EmailField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Email"
+    }))
+    phone = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Phone#"
+    }))
+    
+    Subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Subject"
+    }))
+    Question = forms.CharField(widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "placeholder": "Message"
+    }))
     class Meta:
         model = Question
         fields = ["first_name", "last_name", "email", "phone", "Subject", "Question"]
 
 
+
 class PropertyInquiryForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "First Name"
+    }))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Last Name"
+    }))
+    email = forms.EmailField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Email"
+    }))
+    phone = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Phone#"
+    }))
+    
+    Subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        "class": "form-control form-control-lg form-control-a",
+        "placeholder": "Subject"
+    }))
+    Question = forms.CharField(widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "placeholder": "Message"
+    }))
+    
     class Meta:
         model = PropertyInquiry
         fields = ["first_name", "last_name", "email", "phone", "Subject", "Question"]
